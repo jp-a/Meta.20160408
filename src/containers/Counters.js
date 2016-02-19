0257069ee683e28f17485d3bfdb185abd1ac0de9
Jean-Philippe Avelange <jpa@avelto.com>
2016-02-19 07:13:09 +0100

    refactoring: into separate containers and components

--- src/containers/Counters.js
+++ src/containers/Counters.js
@@ -1,34 +1,6 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router'
-
-class Counter extends Component {
-    constructor( props ) {
-        super( props );
-        this.state = { counter: 0 };
-        this.interval = setInterval( () => this.tick(), 1000 );
-    }
-
-    tick() {
-        console.log( 'tick v' + this.props.version );
-        this.setState( {
-            counter: this.state.counter + this.props.increment
-        } );
-    }
-
-    componentWillUnmount() {
-        clearInterval( this.interval );
-    }
-
-    render() {
-        // throw new Error( 'oops' );
-        return (
-            <h1 style={{ color: this.props.color }}>
-                Counter ( version: { this.props.version }&nbsp;
-                | increment: { this.props.increment } ): { this.state.counter }
-            </h1>
-        );
-    }
-}
+import Counter from '../components/Counter'
 
 export default class Counters extends Component {
     render() {
---
+++ src/containers/Meta.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router'
+
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+
+import Navigation from './Navigation'
+import Nodes from './Nodes'
+import Node from './Node'
+
+class Meta extends Component {
+    constructor( props ) {
+        super( props );
+        this.state = {};
+    }
+
+    render() {
+        console.log( '[Meta]', this.props.params.splat );
+
+        let node = undefined;
+        if ( this.props.params.splat ) {
+            // jpa - 15/02/2016 - 19:11 // lodash refusing to work for whatever reason... reverting to native.
+            // node = _where( this.props.nodes, { _id: this.props.params.splat } )[ 0 ];
+            node = this.props.nodes.filter( ( obj ) => obj._id === this.props.params.splat )[ 0 ];
+            if ( node ) this.state.node = node;
+            else this.state.node = undefined;
+        }
+
+    return <div>
+        <Navigation/>
+
+        <div>Meta{ this.props.params.splat && ( ': ' + this.props.params.splat ) }</div>
+
+        { !node && <Nodes/> }
+        { node &&
+        <Node key={ node._id } index={ 0 } node={ node } actions={ this.props.actions }/> }
+
+        </div>
+    }
+}
+
+// Meta.contextTypes = { router: PropTypes.object };
+import * as NodesActions from '../data/actions/nodes';
+Meta = connect(
+    ( state ) => {
+        return { nodes: state.nodes, syncState: state.syncState }
+    },
+    ( dispatch ) => {
+        return { actions: bindActionCreators( NodesActions, dispatch ) }
+    }
+)( Meta );
+
+export default Meta
